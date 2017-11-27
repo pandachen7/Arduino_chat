fa5fffb610d4b51f5648b9592c7262423bf58e65
pandachen7 <pandaria.chen@gmail.com>
2017-11-27 17:40:20 +0800

    example

---
+++ LoRaTx/lora_useLoratx/lora_useLoratx.ino
@@ -0,0 +1,26 @@
+//Very Simple
+
+#include <LoRaTx.h>
+LoRaTx mylora;
+
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  uint8_t addrCh[] = {0, 0, 23};
+  mylora.setAddrChannel(addrCh);
+
+  mylora.setData("haha uccu");
+  mylora.sendData();
+
+  mylora.setData("x");
+  mylora.sendData();
+
+}
+
+void loop() {
+
+}
