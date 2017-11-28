401c15cb81890aa551bf84137c3a349f1bc3303e
pandachen7 <pandaria.chen@gmail.com>
2017-11-28 13:27:27 +0800

    example

--- LoRaTx/lora_useLoratx/lora_useLoratx.ino
+++ LoRaTx/lora_useLoratx/lora_useLoratx.ino
@@ -12,13 +12,25 @@ void setup() {
 
   uint8_t addrCh[] = {0, 0, 23};
   mylora.setAddrChannel(addrCh);
-
-  mylora.setData("haha uccu");
+  mylora.sendAddrChannel();
+    
+  mylora.setData("haha uccu\n");// not enough space, only set 'haha uc', note that the last of char[] should be 0 or '\0', so this example isn't good
   mylora.sendData();
+  Serial.write("You just send \"");
+  Serial.write(mylora.getData());
+  Serial.write("\"\n");
 
-  mylora.setData("x");
-  mylora.sendData();
+  char myString[] = "directly sending";
+  mylora.sendString(myString);
+  Serial.write("You just send \"");
+  Serial.write(myString);
+  Serial.write("\"\n");
 
+  uint8_t myBytes[] = {49, 50, 51, 52, 53, 0};
+  mylora.sendBytes(myBytes, sizeof(myBytes));
+  Serial.write("You just send \"");
+  Serial.write(myBytes, sizeof(myBytes));
+  Serial.write("\"\n");
 }
 
 void loop() {
